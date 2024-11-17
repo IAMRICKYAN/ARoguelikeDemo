57ff83d71fc31cbb5fd4818d601998ff5f7ce45c
YANZHANYUAN <rickyan.s@foxmail.com>
2024-11-17 10:15:56 +0800

    添加PlayerController相关代码

--- Source/ARogueLikeDemo/Private/SPlayerController.cpp
+++ Source/ARogueLikeDemo/Private/SPlayerController.cpp
@@ -2,5 +2,81 @@
 
 
 #include "SPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(SPlayerController)
 
+void ASPlayerController::TogglePauseMenu()
+{
+	if (PauseMenuInstance && PauseMenuInstance->IsInViewport())
+	{
+		PauseMenuInstance->RemoveFromParent();
+		PauseMenuInstance = nullptr;
+
+		bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+
+		// Single-player only
+		if (GetWorld()->IsNetMode(NM_Standalone))
+		{
+			UGameplayStatics::SetGamePaused(this, false);
+		}
+
+		return;
+	}
+
+	PauseMenuInstance = CreateWidget<UUserWidget>(this, PauseMenuClass);
+	if (PauseMenuInstance)
+	{
+		PauseMenuInstance->AddToViewport(100);
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+
+		// Single-player only
+		if (GetWorld()->IsNetMode(NM_Standalone))
+		{
+			UGameplayStatics::SetGamePaused(this, true);
+		}
+	}
+}
+
+void ASPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	//InputComponent->BindAction("PauseMenu", IE_Pressed, this, &ASPlayerController::TogglePauseMenu);
+
+	if(UEnhancedInputLocalPlayerSubsystem* EnhancedInputLocalPlayerSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		EnhancedInputLocalPlayerSubsystem->AddMappingContext(InputMappingContext, 0);
+	}
+
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(Input_PauseMenu, ETriggerEvent::Triggered, this, &ASPlayerController::TogglePauseMenu);
+	}
+	
+}
+
+void ASPlayerController::SetPawn(APawn* InPawn)
+{
+	Super::SetPawn(InPawn);
+	OnPawnChanged.Broadcast(InPawn);
+}
+
+void ASPlayerController::BeginPlayingState()
+{
+	BlueprintBeginPlayingState();
+}
+
+void ASPlayerController::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	OnPlayerStateReceived.Broadcast(PlayerState);
+}
--- Source/ARogueLikeDemo/Public/SPlayerController.h
+++ Source/ARogueLikeDemo/Public/SPlayerController.h
@@ -6,6 +6,11 @@
 #include "GameFramework/PlayerController.h"
 #include "SPlayerController.generated.h"
 
+class UInputAction;
+class UInputMappingContext;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPawnChanged, APawn*, NewPawn);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStateChanged, APlayerState*, NewPlayerState);
 /**
  * 
  */
@@ -13,5 +18,41 @@ UCLASS()
 class AROGUELIKEDEMO_API ASPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+	protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> PauseMenuClass;
+
+	UPROPERTY()
+	TObjectPtr<UUserWidget> PauseMenuInstance;
 	
+
+	UFUNCTION(BlueprintCallable)
+	void TogglePauseMenu();
+
+	virtual void SetupInputComponent() override;
+
+	UPROPERTY(EditDefaultsOnly, Category="EnhancedInput")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="EnhancedInput|Menu")
+	TObjectPtr<UInputAction> Input_PauseMenu;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnPawnChanged OnPawnChanged;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPlayerStateChanged OnPlayerStateReceived;
+
+	virtual void SetPawn(APawn* InPawn) override;
+
+	/* 当PlayerController准备好开始游戏的时候调用, 初始化UI之类与游戏数据有关的对象的好时机
+			(特别是在多人游戏中客户端可能没有足够多的信息，因为PlayerState还没有收到) */
+	virtual void BeginPlayingState() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void BlueprintBeginPlayingState();
+
+	void OnRep_PlayerState() override;
 };
